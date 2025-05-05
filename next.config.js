dd9c7ebee70fec3406836175e7594f16817f1335
nini <ninii.chirgadze@gmail.com>
2025-05-06 00:57:13 +0400

    too many requests bug fix ?

--- next.config.js
+++ next.config.js
@@ -26,6 +26,15 @@ const nextConfig = {
       ],
     },
   ],
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/en',
+        permanent: true,
+      },
+    ];
+  },
 }
 
 module.exports = nextConfig
